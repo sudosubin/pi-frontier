b52561457caf90664b1618493c8ac4d70427a46f
sudosubin <sudosubin@gmail.com>
2026-02-18 03:27:59 +0900

    ci(root): Add release-it for multi-package pipeline

---
+++ .release-it.ts
@@ -0,0 +1,38 @@
+import path from "node:path";
+
+const scope = path.basename(process.cwd());
+
+export default {
+  git: {
+    requireBranch: ["main"],
+    requireCleanWorkingDir: true,
+    commitsPath: ".",
+    commitMessage: "release(${npm.name}): v${version}",
+    tagName: "${npm.name}@${version}",
+    tagAnnotation: "${npm.name}@${version}",
+    tagMatch: "${npm.name}@*",
+    getLatestTagFromAllRefs: true,
+    pushArgs: ["--follow-tags"],
+  },
+  npm: {
+    publish: true,
+    skipChecks: true,
+  },
+  github: {
+    release: true,
+    releaseName: "${npm.name}@${version}",
+    autoGenerate: false,
+  },
+  plugins: {
+    "@release-it/conventional-changelog": {
+      preset: {
+        name: "conventionalcommits",
+        scope,
+        scopeOnly: true,
+        bumpStrict: true,
+      },
+      context: { linkCompare: false },
+      infile: "CHANGELOG.md",
+    },
+  },
+};
